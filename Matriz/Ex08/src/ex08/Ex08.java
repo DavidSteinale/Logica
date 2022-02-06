bb769063e037bc0baa2eea506278ca8f214c3916
David Stein <stein.rs@gmail.com>
2022-02-06 07:38:22 -0300

    Exercicio 08

---
+++ Matriz/Ex08/src/ex08/Ex08.java
@@ -0,0 +1,41 @@
+package ex08;
+
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+public class Ex08 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int tamanho = 4;
+        int soma = 0;
+
+        int[][] matriz = new int[tamanho][tamanho];
+
+        for (int x = 0; x < tamanho; x++) {
+            for (int y = 0; y < tamanho; y++) {
+                matriz[x][y] = random.nextInt(100);
+            }
+        }
+        System.out.println("--- MATRIZ ---");
+        System.out.println();
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                System.out.print(matriz[x][y] + " ");
+            }
+            System.out.println();
+        }
+        for (int x = 0; x < tamanho; x++) {
+            for (int y = 0; y < tamanho; y++) {
+                if(x<y){
+                    soma+=matriz[x][y];
+                }                
+            }
+        }
+        System.out.println();
+        System.out.println("Soma dos elementos que estão cima da Dp: " +soma);
+        System.out.println();
+    }
+
+}
