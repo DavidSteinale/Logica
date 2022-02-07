8ca47ccfa71b7edfee63bc519b3a77d4ba0b67f5
David Stein <stein.rs@gmail.com>
2022-02-07 17:37:37 -0300

    Exercicio 15

---
+++ Matriz/Ex14/src/ex14/Ex14.java
@@ -0,0 +1,37 @@
+
+package ex14;
+
+import java.util.Random;
+
+public class Ex14 {
+    public static void main(String[] args) {
+         Random random = new Random();
+
+        int tamanho = 10;
+        
+        int[][] matriz = new int[tamanho][tamanho];
+
+        for (int x = 0; x < tamanho; x++) {
+            for (int y = 0; y < tamanho; y++) {
+                int numero = random.nextInt(50);
+                for(int[] valor: matriz){
+                    for(int a : valor){
+                        if(a != numero){
+                            matriz[x][y]= numero;                            
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("--- Números Sortiados ---");
+        System.out.println();
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                System.out.print(matriz[x][y] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }    
+}
---
+++ Matriz/Ex15/src/ex15/Ex15.java
@@ -0,0 +1,58 @@
+package ex15;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex15 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int coluna = 10;
+        int linha = 5;
+        int soma = 0;
+
+        char[][] resposta = new char[linha][coluna];
+        char[] gabarito = {'a', 'b', 'c', 'd', 'b', 'b', 'c', 'c', 'a', 'd'};
+        int[] resultado = new int[linha];
+
+        //MATRIZ PARA RECEBER AS NOTAS
+        System.out.println("Informe às respostas:");
+        for (int x = 0; x < resposta.length; x++) {
+            System.out.println("------");
+            for (int y = 0; y < resposta[x].length; y++) {
+                System.out.print("Resposta da questão número " + (y + 1) + " do aluno " + (x + 1) + " : ");
+                String l = scanner.nextLine();
+                resposta[x][y] = l.charAt(0);
+            }
+        }
+
+        //MATRIZ PARA MOSTRAR AS NOTAS DIGITADAS
+        System.out.println();
+        System.out.println("--- RESPOSTAS DO ALUNOS ---");
+        System.out.println();
+        for (int x = 0; x < resposta.length; x++) {
+            for (int y = 0; y < resposta[x].length; y++) {
+                System.out.print(resposta[x][y] + " ");
+            }
+            System.out.println();
+        }
+
+        //MATRIZ PARA FAZER A CONPARAÇÃO DAS NOTAS DIGITADAS COM O GABARITO
+        for (int x = 0; x < resposta.length; x++) {
+            for (int y = 0; y < resposta[0].length; y++) {
+                if (resposta[x][y] == gabarito[y]) {
+                    soma++;
+                    resultado[x] = soma;
+                }
+            }
+            soma = 0;
+        }
+
+        //MOSTRA O VETOR RESULTADO COM A PONTUAÇÃO DE CADA ALUNO.
+        System.out.println();
+        for (int x = 0; x < resultado.length; x++) {
+            System.out.println("Pontuação do aluno " + (x + 1) + " é " + resultado[x] + ".");
+        }
+    }
+}
